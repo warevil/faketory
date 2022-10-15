2de6d6cf4610e18b5839bbf61436ee731171fef6
warevil <60890246+warevil@users.noreply.github.com>
2022-10-15 00:48:56 -0500

    Add more tests and improve docs

--- tests/test_faketory.py
+++ tests/test_faketory.py
@@ -11,19 +11,22 @@ class _Factory:
         def test_quantity_below_1_returns_ERROR(self):
             number = self.fake.pyint(min_value=-100, max_value=0)
             with raises(ValueError):
-                self.factory(_quantity=number)
+                self.factory(_qty=number)
 
         def test_quantity_below_1_returns_ERROR_even_if_list(self):
             number = self.fake.pyint(min_value=-100, max_value=0)
             with raises(ValueError):
-                self.factory(_quantity=number, _list=True)
+                self.factory(_qty=number, _list=True)
 
 
 class TestFactory(_Factory.TestCase):
-    def test_blank_init_returns_one_element(self):
-        element = self.factory()
+    def _assert_fields(self, element):
         assert isinstance(element.email, str)
         assert isinstance(element.name, str)
+
+    def test_blank_init_returns_one_element(self):
+        element = self.factory()
+        self._assert_fields(element)
 
     def test_custom_values_override_default_ones_returns_one_element(self):
         email = self.fake.email()
@@ -34,54 +37,47 @@ class TestFactory(_Factory.TestCase):
 
     def test_quantity_blank_returns_one_element_if_generator(self):
         element = self.factory(_type='generator')
-        assert isinstance(element.email, str)
-        assert isinstance(element.name, str)
+        self._assert_fields(element)
 
     def test_quantity_blank_returns_one_element_if_list(self):
         element = self.factory(_type='list')
-        assert isinstance(element.email, str)
-        assert isinstance(element.name, str)
+        self._assert_fields(element)
 
     def test_quantity_blank_returns_one_element_if_set(self):
         element = self.factory(_type='set')
-        assert isinstance(element.email, str)
-        assert isinstance(element.name, str)
+        self._assert_fields(element)
 
     def test_quantity_2_or_more_returns_many_elements_as_list_by_default(self):
-        number_of_elements = self.fake.pyint(min_value=2, max_value=20)
-        elements = self.factory(_quantity=number_of_elements, _type='list')
-        assert len(elements) == number_of_elements
+        quantity = self.fake.pyint(min_value=2, max_value=20)
+        elements = self.factory(_qty=quantity, _type='list')
+        assert len(elements) == quantity
         for element in elements:
-            assert isinstance(element.email, str)
-            assert isinstance(element.name, str)
+            self._assert_fields(element)
 
     def test_quantity_2_or_more_returns_many_elements_if_generator_when_consumed(self):
-        number_of_elements = self.fake.pyint(min_value=2, max_value=20)
-        elements = self.factory(_quantity=number_of_elements, _type='generator')
+        quantity = self.fake.pyint(min_value=2, max_value=20)
+        elements = self.factory(_qty=quantity, _type='generator')
         element = next(elements)  # 1st element to verify it is a generator
         count = 1
         for element in elements:
-            assert isinstance(element.email, str)
-            assert isinstance(element.name, str)
+            self._assert_fields(element)
             count += 1
 
-        assert count == number_of_elements
+        assert count == quantity
 
     def test_quantity_2_or_more_returns_many_elements_as_a_list_if_list(self):
-        number_of_elements = self.fake.pyint(min_value=2, max_value=20)
-        elements = self.factory(_quantity=number_of_elements, _type='list')
-        assert len(elements) == number_of_elements
+        quantity = self.fake.pyint(min_value=2, max_value=20)
+        elements = self.factory(_qty=quantity, _type='list')
+        assert len(elements) == quantity
         for element in elements:
-            assert isinstance(element.email, str)
-            assert isinstance(element.name, str)
+            self._assert_fields(element)
 
     def test_quantity_2_or_more_returns_many_elements_as_a_set_if_set(self):
-        number_of_elements = self.fake.pyint(min_value=2, max_value=20)
-        elements = self.factory(_quantity=number_of_elements, _type='set')
-        assert len(elements) == number_of_elements
+        quantity = self.fake.pyint(min_value=2, max_value=20)
+        elements = self.factory(_qty=quantity, _type='set')
+        assert len(elements) == quantity
         for element in elements:
-            assert isinstance(element.email, str)
-            assert isinstance(element.name, str)
+            self._assert_fields(element)
 
 
 class TestSampleFactory(TestFactory):
@@ -97,9 +93,38 @@ class TestSampleFactory(TestFactory):
 class TestSampleDjangoFactory(TestFactory):
     factory = SampleDjangoFactory
 
-    def test_save_returns_triggers_resolver(self):
-        element = self.factory(_resolver='save')
+    def _assert_sample_django_fields(self, element):
         assert isinstance(element.age, int)
         assert isinstance(element.email, str)
         assert isinstance(element.name, str)
         assert element.year == 3000
+
+    def test_save_one_triggers_resolver(self):
+        element = self.factory(_resolver='save')
+        self._assert_sample_django_fields(element)
+
+    def test_save_one_triggers_resolver_if_generator(self):
+        element = self.factory(_resolver='save', _type='generator')
+        self._assert_sample_django_fields(element)
+
+    def test_save_one_triggers_resolver_if_set(self):
+        element = self.factory(_resolver='save', _type='set')
+        self._assert_sample_django_fields(element)
+
+    def test_save_many_triggers_resolver(self):
+        quantity = self.fake.pyint(min_value=2, max_value=20)
+        elements = self.factory(_qty=quantity, _resolver='save')
+        for element in elements:
+            self._assert_sample_django_fields(element)
+
+    def test_save_many_triggers_resolver_if_generator(self):
+        quantity = self.fake.pyint(min_value=2, max_value=20)
+        elements = self.factory(_qty=quantity, _resolver='save', _type='generator')
+        for element in elements:
+            self._assert_sample_django_fields(element)
+
+    def test_save_many_triggers_resolver_if_set(self):
+        quantity = self.fake.pyint(min_value=2, max_value=20)
+        elements = self.factory(_qty=quantity, _resolver='save', _type='set')
+        for element in elements:
+            self._assert_sample_django_fields(element)
