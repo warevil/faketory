5640e016d630850c4405acbaa5ea3aaefc54b67b
jg@warevil.dev <jg@warevil.dev>
2025-04-25 00:23:19 -0500

    chore: bump faker and replace poetry with uv (#3)

--- validate.sh
+++ validate.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-source _faketory/bin/activate
-
-
-black --line-length=95 --skip-string-normalization faketory/ tests/
-flake8 faketory/ tests/
-isort faketory/ tests/
-pytest --cov=faketory tests
+uv run ruff check --fix
+uv run pytest --cov=faketory tests
 rm -f .coverage*;
