5235ab1ee379d66d16f60abe8862d05fda9ff11a
JG <jg@warevil.dev>
2025-04-25 00:16:46 -0500

    chore: bump faker and replace poetry with uv

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Create virtual environment and then activate it
-python -m venv _faketory
-source _faketory/bin/activate
+python -m venv .venv
+source .venv/bin/activate
 
 pip install --upgrade pip
-# Install poetry, then use it to install dependencies
-pip install poetry
-poetry install
-poetry run pre-commit install
+# Install uv
+pip install uv
+uv sync
